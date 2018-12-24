d3358dd3181f81f775a825f853e17311805f2dd6
Niall O Connor <nialloconnor@nialls-mbp.home>
2018-12-24 14:08:47 +0000

    feat: added serverless functions

---
+++ handler.js
@@ -0,0 +1,21 @@
+"use strict";
+
+module.exports.hello = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      input: event
+    })
+  };
+};
+
+module.exports.imageResize = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "resized your image",
+      input: event
+    })
+  };
+};
